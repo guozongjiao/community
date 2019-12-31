ccb94d27e78e9f6291bfc360cdf9a66af6dfbef8
gzj <gzj@qq.com>
2019-12-31 21:51:40 +0800

    init repo

---
+++ src/main/java/left/majiang/communlty/communlty/CommunltyApplication.java
@@ -0,0 +1,13 @@
+package left.majiang.communlty.communlty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunltyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunltyApplication.class, args);
+    }
+
+}
